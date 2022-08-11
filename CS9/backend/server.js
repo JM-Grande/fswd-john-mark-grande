afb9d5f21fef412009053105ba2fade695803880
JM Grande <grandejmofficial@gmail.com>
2022-08-11 14:00:12 +0800

    CS9 Node JS and Express

---
+++ CS9/backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import items from "./data/items.js";
+
+dotenv.config();
+
+const APP = express();
+const PORT = process.env.PORT || 8000;
+
+APP.get("/", (request, response) => {
+  response.send("Authorized");
+});
+
+APP.get("/api/v1/items", (request, response) => {
+  response.json(items);
+});
+
+APP.get("/api/v1/items/:id", (request, response) => {
+  const item = items.find((item) => item._id === request.params.id);
+  response.json(item);
+});
+
+APP.listen(PORT, console.log(`Server is running at port ${PORT}`));
